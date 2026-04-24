0747b285fca45be0261c4139676e4fd12229ea9f
Jonatham Ampau Zea <z_tjalezea@outlook.com>
2026-04-24 09:36:14 -0500

    :package: binaries from MA_autoregresive

--- run_trains.py
+++ run_trains.py
@@ -56,7 +56,7 @@ from datetime import datetime
 # PARAM_ITERATE = "N_LAST_STATES_INIT"
 # PARAMS = [2, 3, 4, 6, 12, 16]
 
-EXPERIMENT_NAME = "LA_mcSelect"
+EXPERIMENT_NAME = "MA_autoregresive"
 PARAM_ITERATE = "N_LAST_STATES_INIT"
 PARAMS = [2, 3, 4, 6, 12, 16]
 
