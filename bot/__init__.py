a10b25c388b0cb27a4cf697999328fee22f516d3
Jonathan o9010 Zea <z_tjalezea@outlook.com>
2026-04-24 19:58:11 -0500

    :alembic: evaluate minimax vs loss-BT. Minimax d2 wins 98%.

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,5 @@
 from .CNN_bot import Quarto_bot as CNNQuartoBot
 from .CNN_autoreg_bot import Quarto_bot as CNNAutoregQuartoBot
+from .minimax_bot import MinimaxBot
 
-__all__ = ["CNNQuartoBot", "CNNAutoregQuartoBot"]
+__all__ = ["CNNQuartoBot", "CNNAutoregQuartoBot", "MinimaxBot"]
